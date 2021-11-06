707a7c12ba3dc03702a32ce0c8bef8537fc9a483
ilonaGold <songa17@inbox.lv>
2021-11-06 16:04:23 +0200

    Added extra  effects: inline SVG changes colors on click

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const hex = document.querySelector(".hex");
-const btn = document.querySelector(".btn-generate");
+const btn = document.querySelector(".btn");
+const colorPrime = document.getElementsByClassName("color-prime");
 
 const generateColor = () => {
     const randomColor = Math.random().toString(16).substring(2, 8);
     document.body.style.backgroundColor = "#" + randomColor;
     hex.textContent = "#" + randomColor;
+    for (let i=0; i < colorPrime.length; i++) {
+        colorPrime[i].style.fill = "#" + randomColor;
+    }
 };
 
 btn.addEventListener("click", generateColor);
 
 generateColor();
+ 
